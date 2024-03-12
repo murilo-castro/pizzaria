52b6b628d622d1f0bbb40917b7dcc74ab24a3ad7
Murilo Castro <murilo.65@gmail.com>
2024-03-12 18:47:45 -0300

    configuring prisma and creating users schema

--- src/sever.ts
+++
@@ -1,9 +0,0 @@
-import express, {Request, Response, NextFunction } from "express";
-import router from "./routes";
-
-const app = express();
-app.use(express.json());
-
-app.use(router)
-
-app.listen(3333, () => console.log("Servidor online na porta 3333, http://localhost:3333"));
