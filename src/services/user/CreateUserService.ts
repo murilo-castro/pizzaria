8411cc47f51a4e61e97ed2ba17096c42d868975e
Murilo Castro <murilo.65@gmail.com>
2024-03-14 23:53:41 -0300

    crerate controller, service, and register user of data base-feature#6

---
+++ src/services/user/CreateUserService.ts
@@ -0,0 +1,45 @@
+import prismaClient from "../../prisma"
+
+interface UserRequest {
+  name: string,
+  email: string,
+  password: string
+}
+
+class CreateUserService {
+  async execute({ name, email, password }: UserRequest) {
+
+    if (!email) {
+      throw new Error("Email Incorrect!")
+    }
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email
+      }
+    })
+
+    if (userAlreadyExists) {
+      throw new Error("user already exists!")
+    }
+
+
+
+    const user = await prismaClient.user.create({
+      data: {
+        name,
+        email,
+        password
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true
+      }
+    })
+
+    return user;
+  }
+}
+
+export { CreateUserService}
