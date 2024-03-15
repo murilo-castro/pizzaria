890a64989964bab1eb99222638cb0212a8aeeec7
Murilo Castro <murilo.65@gmail.com>
2024-03-15 20:16:18 -0300

    created auth user and jwt-feature#8

---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,51 @@
+import prismaClient from "../../prisma"
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken"
+
+interface AuthRequest {
+  email: string,
+  password: string
+}
+
+class AuthUserService {
+  async execute( { email, password}: AuthRequest) {
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email
+      }
+    })
+
+    if (!user) {
+      throw new Error("Usuário/Senha incorretos!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Usuário/Senha incorretos!")
+    }
+
+    const jwtsecret = String(process.env.JWT_SECRET)
+
+    const token = sign(
+      {
+      name: user.name,
+      email: user.email,
+    },
+    jwtsecret,
+    {
+      subject: user.id,
+      expiresIn: '30d'
+    }
+    )
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token
+    }
+  }
+}
+
+export { AuthUserService }
